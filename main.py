2cb63d633d0ac9dce68e74a92413485dc4d5ad30
bharat8989 <kadamb208@gmail.com>
2026-06-20 23:17:22 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+print("hello world")
+print(23+45)
+print("namaste duniya kya hall hai namestey")
+#how are you 
+## let's do some things in python 
+"""this is multiline comment 
+this is multiline comment
+multiline comment"""
+#variable 
+#variable maje data store karayla use hota ahe data satvanesathi vapare jate name 
+age =23 
+name="bharat"
+print(age)
+print(name)
