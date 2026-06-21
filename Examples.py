2cfb6a45fb3b84a87357a0af35173fe197e9cce0
bharat8989 <kadamb208@gmail.com>
2026-06-21 16:01:36 +0530

    operator

---
+++ Examples.py
@@ -0,0 +1,32 @@
+"""Sum of two Numbers"""
+#using the user input 
+
+# num1=int(input("enter the first number:"))
+# num2=int(input("enter the second number :"))
+# # num1=int(num1)
+# sum =num1+num2
+# #the valule of the give num1 and num2
+
+# print("the num1 values is :",num1, "and num2 value is :",num2)
+
+# print("the sum of the num1 and num2 is :",sum)
+
+# using the f-String (modern way))
+print("use the f-string")
+
+name = "Bharat"
+age = 23
+print(f"my name is {name} and my age is {age} year old")
+
+# float formatting
+print(f"Float formatting ")
+marks =23.2
+print(f"your marks is {marks:}")
+
+
+#Raw String 
+print("raw\nString  ")
+print("HI\nMY NAME IS BHARAT \n MY FATHER NAME \t IS NAGNATH KADAM ")
+print("D:\Python\DataTypes.py")
+#operator
+print("Operator")
---
+++ Operator.py
@@ -0,0 +1,41 @@
+#operator  
+"""Types of operators in python """
+a=10
+b=20
+print(a+b)
+print(b-a)
+print(b*a)
+print(b**a)
+print(b/a)
+print(b//a)
+print(b%a)
+print(b-a)
+
+print("comparison operator")
+print(b==a)
+print(b>=a)
+print(b<=a)
+print(b>a)
+print(b<a)
+
+print(f"logical operators ")
+age =23
+print(age > 18 and age < 30 )
+print(age > 18 or age < 50)
+print((not(age > 18)))
+
+print(r"Assignment operators ")
+num=12
+num1=20
+num =num+12
+print(num)
+num=num+12 # 36
+print(num)
+
+print("A">"B")
+
+
+""" operation  logical """
+# and operation all true result is true and one false result is false 
+#or operations one result is true than result is true 
+# not operations true ahe tar false denar 
