f86c3bd66ea141581c8342674454183dbf998a90
bharat8989 <kadamb208@gmail.com>
2026-06-27 12:08:12 +0530

    add the todo list using the python

--- Flask/app.py
+++ Flask/app.py
@@ -1,42 +1,94 @@
-from flask import Flask, request
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"{self.sno} - {self.title}"
+
+
+# Home Page
+@app.route("/", methods=["GET", "POST"])
+def home():
 
     if request.method == "POST":
-        username = request.form["username"]
-        return f"Welcome {username}"
 
-    return '''
-        <form method="POST">
-            <input type="text" name="username">
-            <input type="submit">
-        </form>
-    '''
+        title = request.form["title"]
+        desc = request.form["desc"]
+
+        todo = Todo(
+            title=title,
+            desc=desc
+        )
+
+        db.session.add(todo)
+        db.session.commit()
+
+        return redirect("/")
+
+    allTodo = Todo.query.all()
+
+    return render_template(
+        "index.html",
+        allTodo=allTodo
+    )
+
+
+# Delete Todo
+@app.route("/delete/<int:sno>", methods=["GET", "POST"])
+def delete(sno):
+
+    todo = Todo.query.filter_by(sno=sno).first()
+
+    if not todo:
+        return redirect("/")
+
+    if request.method == "POST":
+        db.session.delete(todo)
+        db.session.commit()
+        return redirect("/")
+
+    return render_template("delete.html", todo=todo)
+
+
+# Update Todo
+@app.route("/update/<int:sno>", methods=["GET", "POST"])
+def update(sno):
+
+    todo = Todo.query.filter_by(sno=sno).first()
+
+    if request.method == "POST":
+
+        todo.title = request.form["title"]
+        todo.desc = request.form["desc"]
+
+        db.session.commit()
+
+        return redirect("/")
+
+    return render_template(
+        "update.html",
+        todo=todo
+    )
+
 
 if __name__ == "__main__":
+
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True)
-
-
-# from flask import Flask, request
-
-# app = Flask(__name__)
-
-# @app.route("/login", methods=["GET", "POST"])
-# def login():
-
-#     if request.args.get == "GET":
-#         username = request.args.get["username"]
-#         return f"Welcome {username}"
-
-#     return '''
-#         <form method="GET">
-#             <input type="text" name="username">
-#             <input type="submit">
-#         </form>
-#     '''
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
