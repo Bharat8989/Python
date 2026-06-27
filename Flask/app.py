597048da84e9f3fed2219c37e0ddff0f1f6c2ed9
bharat8989 <kadamb208@gmail.com>
2026-06-27 14:47:24 +0530

    change the port

--- Flask/app.py
+++ Flask/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
@@ -91,4 +92,7 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-    app.run(debug=False)
+    port=int(os.environ.get("PORT",5000))
+
+
+    app.run(host="0.0.0.0",port=port)
