8b9cc1d02cc09dbae9ca866f6f7ad08c3326912c
bharat8989 <kadamb208@gmail.com>
2026-06-29 21:55:15 +0530

    add the crud operations

--- PythonWeb/Flask_Backend/controller/user_controller.py
+++ PythonWeb/Flask_Backend/controller/user_controller.py
@@ -1,9 +1,25 @@
-from flask import Blueprint
-from model.user_model import user_model 
-# obj =user_model()
-user_bp = Blueprint("user", __name__ ,url_prefix='/user')
+from flask import Blueprint, request
+from model.user_model import user_model
 
-@user_bp.route("/getall")
+user_bp = Blueprint("user", __name__, url_prefix="/user")
+
+
+# GET ALL USERS
+@user_bp.route("/getall", methods=["GET"])
 def user_getall_controller():
-    obj=user_model()
+    obj = user_model()
     return obj.user_getall_model()
+
+
+# INSERT USER
+@user_bp.route("/addone", methods=["POST"])
+def user_addone_controller():
+    obj = user_model()
+    return obj.user_addone_model(request.form)
+
+# Update user
+
+@user_bp.route("/update", methods=["PUT"])
+def user_update_controller():
+    obj = user_model()
+    return obj.user_update_model(request.form)
--- PythonWeb/Flask_Backend/model/user_model.py
+++ PythonWeb/Flask_Backend/model/user_model.py
@@ -1,24 +1,71 @@
 import mysql.connector
-import json
-class user_model():
+from flask import jsonify
+
+
+class user_model:
+
     def __init__(self):
-        # connections code 
-        try:
-            self.con=mysql.connector.connect(host='localhost',user='root',password='Bharat@1297',database='flaskdb')
-            self.cur=self.con.cursor(dictionary=True)
 
-            print("connection establish")
-        except:
-            print("some error")
-   
+        self.con = mysql.connector.connect(
+            host="localhost", user="root", password="Bharat@1297", database="flaskdb"
+        )
 
+        self.con.autocommit = True
+        self.cur = self.con.cursor(dictionary=True)
+
+    # GET ALL USERS
     def user_getall_model(self):
-        #query executions code 
+
         self.cur.execute("SELECT * FROM users")
-        result=self.cur.fetchall()
-        if len(result)>0:
-            return json.dumps(result)
 
+        result = self.cur.fetchall()
 
+        if len(result) > 0:
+            return jsonify(result)
+
+        return jsonify({"message": "No Data Found"}), 404
+
+    # INSERT USER
+    def user_addone_model(self, data):
+
+        self.cur.execute(
+            """
+            INSERT INTO users(name,email,phone,role)
+            VALUES(%s,%s,%s,%s)
+            """,
+            (data["name"], data["email"], data["phone"], data["role"]),
+        )
+
+        self.con.commit()
+
+        return jsonify({"status": 1, "message": "User Added Successfully"})
+
+   # Update User
+    def user_update_model(self, data):
+
+        self.cur.execute(
+        """
+        UPDATE users
+        SET
+            name=%s,
+            email=%s,
+            phone=%s,
+            role=%s
+        WHERE id=%s
+        """,
+        (
+            data["name"],
+            data["email"],
+            data["phone"],
+            data["role"],
+            data["id"]
+        )
+    )
+
+        self.con.commit()
+
+        if self.cur.rowcount > 0:
+            return "User Updated Successfully"
         else:
-            return "no data found"
+            return "Nothing to Update"
+    
