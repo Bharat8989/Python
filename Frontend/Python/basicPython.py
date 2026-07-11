4e02b7c75c34662edeb11b2f8e1965a90ff85b55
bharat8989 <kadamb208@gmail.com>
2026-07-11 13:28:44 +0530

    python basic problem solving

---
+++ Frontend/Python/basicPython.py
@@ -0,0 +1,43 @@
+def print_range_1_10():
+    for i in range(1,10):
+        print(i)
+        
+        
+def print_range_10_1():
+    for i in range(10,0,-1):
+        print(i)
+
+
+
+def even_odd(num):
+    if(num%2==0):
+        print('this is the even number:',num)
+    else:
+        print('this is the odd number:',num)
+            
+
+def print_table(numbers=5):
+    for i in range(1,11):
+        print(i*numbers)
+        
+        
+def sum_natural_number(num):
+    total=(num * (num+1)) // 2
+    print(total)
+    
+    
+    
+# using the for loop
+
+def sum_using_for_loop(n):
+    total_sum = 0
+    for i in range(1, n+1):
+        # total_sum += i  
+        total_sum=total_sum + i
+    print('total sum of natural numbers using the for loop:',total_sum)
+
+import math 
+def factorial1(num):
+    result=math.factorial(num)
+    # print(result)
+    print('factorial:',result)
---
+++ Frontend/Python/main.py
@@ -0,0 +1,35 @@
+from basicPython import print_range_1_10,print_range_10_1,even_odd,print_table
+from basicPython import sum_natural_number,factorial1,sum_using_for_loop
+print('range 1 to 10 :')
+print_range_1_10()
+print('range 10 to 1:')
+print_range_10_1()
+
+print('even and odd numbers:')
+even_odd(45)
+even_odd(40)
+
+print('tables :')
+
+print_table(25)
+print('default table print:')
+print_table()
+
+print('sum of natural numbers:')
+
+sum_natural_number(10)
+
+
+sum_using_for_loop(20)
+
+
+
+sum_using_for_loop(10)
+
+# import math
+# result = math.factorial(5)
+# print('factorial :',result)
+
+
+factorial1(5)
+
