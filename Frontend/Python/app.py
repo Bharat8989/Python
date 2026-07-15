99744917701e23c8a9a905b6f66acd9576de2ee4
bharat8989 <kadamb208@gmail.com>
2026-07-15 23:04:28 +0530

    html fiels add

--- Frontend/Python/app.py
+++ Frontend/Python/app.py
@@ -1,22 +1,25 @@
-def counting_numbers():
-    numbers=1220.41
+# def counting_numbers():
+#     numbers=1220.41
 
-    name='python'
+#     name='python'
 
-    print(len(str(numbers)))
-    print(len(name))
+#     print(len(str(numbers)))
+#     print(len(name))
     
-# def counting_char(name):
-#     for i in range(0,name+1):
-#         print(i)
+# # def counting_char(name):
+# #     for i in range(0,name+1):
+# #         print(i)
         
-# counting_char('python')
+# # counting_char('python')
 
-def square_numbers(n):
-    print('the square number using the loop')
-    for i in range(1,n+1):
-        print(i ** 2)
-        
+# def square_numbers(n):
+#     print('the square number using the loop')
+#     for i in range(1,n+1):
+#         print(i ** 2)
+
+from decorators import about
+
+about()
     
 
 
--- Frontend/Python/decorators.py
+++ Frontend/Python/decorators.py
@@ -14,21 +14,34 @@
 
 
 
-def check_role(required_role):
-    def decorator(func):
-        def wrapper(user_role):
-            if user_role== required_role:
-                return func(user_role)
-            else:
-                print("Access Denied! You do not have the required role.")
+# def check_role(required_role):
+#     def decorator(func):
+#         def wrapper(user_role):
+#             if user_role== required_role:
+#                 return func(user_role)
+#             else:
+#                 print("Access Denied! You do not have the required role.")
         
-        return wrapper
-    return decorator
+#         return wrapper
+#     return decorator
 
-@check_role(required_role="admin")
+# @check_role(required_role="admin")
 
-def delete_database(user_role):
-    print("Database deleted successfully!")
+# def delete_database(user_role):
+#     print("Database deleted successfully!")
     
-delete_database("admin")  # Output: Database deleted successfully!
-# delete_database("user")   # Output: Access Denied! You do not have the required
+# delete_database("admin")  # Output: Database deleted successfully!
+# # delete_database("user")   # Output: Access Denied! You do not have the required
+
+
+def home(func):
+    def wrapper():
+        print('this is print wrapper method ')  # first output
+        func()
+        print('last  output')
+    return wrapper
+
+@home
+def about():
+    print('run the about files ')
+# about()
