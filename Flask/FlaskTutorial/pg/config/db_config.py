6701dd1dd566134813a1afdffaf69c4823912c10
bharat8989 <kadamb208@gmail.com>
2026-08-17 18:43:06 +0530

    add one more folder pg

---
+++ Flask/FlaskTutorial/pg/config/db_config.py
@@ -0,0 +1,9 @@
+class Config:
+
+    SQLALCHEMY_DATABASE_URI = (
+        "mysql+pymysql://root:Bharat%401297@localhost/pg"
+    )
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SQLALCHEMY_ECHO = True
